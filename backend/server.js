ad0881ef4a88e459e60e1f5babca3f5f612568de
Nick <Echophase44@github.com>
2023-06-07 13:35:50 -0400

    Started to build out the front end.

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const workoutRoutes = require("./routes/workouts");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
